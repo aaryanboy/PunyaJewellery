d1225d25f527fa496c8344c1c0da563224e3d706
aaryanboy <aaryanboy12@gmail.com>
2025-03-25 10:00:35 +0545

    test

--- src/components/contactuspagekodata/Map.tsx
+++ src/components/contactuspagekodata/Map.tsx
@@ -1,4 +1,5 @@
 // components/Map.tsx
+import { h1 } from 'framer-motion/client';
 import React from 'react';
 
 interface MapProps {
@@ -10,22 +11,7 @@ const Map: React.FC<MapProps> = ({ location, apiKey }) => {
   const mapSrc = `https://www.google.com/maps/embed/v1/place?q=${encodeURIComponent(location)}&key=${apiKey}`;
   
   return (
-    <section className="contact-us container mx-auto p-4">
-      <h2 className="text-2xl font-semibold mb-4">Our Location</h2>
-
-      {/* Google Map Embed */}
-      <div className="map-container mb-6">
-        <iframe
-          src={mapSrc}
-          width="100%"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          className="rounded-lg shadow-lg"
-        ></iframe>
-      </div>
-    </section>
+   <div className="relative h-96">hello world</div>
   );
 };
 
